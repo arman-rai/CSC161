3140c4be8cde733cc8558f4a18ebddc67154231f
arman-rai <118722334+arman-rai@users.noreply.github.com>
2024-06-01 12:33:36 +0545

    Update DMlength.cpp

--- DMlength.cpp
+++ DMlength.cpp
@@ -5,7 +5,7 @@ int main() {
     int feet, inches;
     char ch;
     string line;
-    // cout << "ENter two ints seperated by one or more spaces" <<endl;
+    // cout << "Enter two ints seperated by one or more spaces" <<endl;
     // cin >> feet >> inches;
     // all non-delimited character lai linxa (ex: spaces)
     // cin is a object and get() is a method
